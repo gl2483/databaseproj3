5ad97328752e06236c891f33e5f22dc4adde353a
KK <KK@dyn-160-39-206-142.dyn.columbia.edu>
2015-11-30 14:34:16 -0500

    parameter check

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,22 @@
 
 public class Application {
 	public static void main(String[] args){
+		if(args.length != 3){
+			System.out.println("Usage: ./run.sh <File name> <min support> <min confidence>");
+			return;
+		}
 		String filename = args[0];
 		double support = Double.parseDouble(args[1]);
 		double conf = Double.parseDouble(args[2]);
+		
+		if(support > 1 || support <= 0){
+			System.out.println("Min support has to be in range 0 < min support <= 1");
+			return;
+		}
+		if(conf > 1 || conf <= 0){
+			System.out.println("Min confidence has to be in range 0 < min confidence <= 1");
+			return;
+		}
 
 		final long start = System.currentTimeMillis();
 		APriori a = new APriori(filename, support, conf);
